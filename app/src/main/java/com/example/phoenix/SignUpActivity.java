b16492b734cf2f35f0531ba19427f3d85ab15684
Medhat <medhatassem.lf@gmail.com>
2021-01-11 10:39:45 +0200

    SignUpActivity

---
+++ app/src/main/java/com/example/phoenix/SignUpActivity.java
@@ -0,0 +1,38 @@
+package com.example.phoenix;
+
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    String tag;
+    protected void ShowFragment (Fragment fragment , String tag){
+        this.tag = tag;
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container_fragment, fragment , tag).addToBackStack(null).commit();
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if(tag == "SignUpFirstFragment")
+                    finish();
+            }
+        };
+        this.getOnBackPressedDispatcher().addCallback(this, callback);
+        //SignUpFirstFragment Open
+        ShowFragment(new SignUpFirstFragment() , "SignUpFirstFragment");
+    }
+}
--- app/src/main/java/com/example/phoenix/WelcomeActivity.java
+++ app/src/main/java/com/example/phoenix/WelcomeActivity.java
@@ -2,13 +2,31 @@ package com.example.phoenix;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    Button sign_up_btn,
+    login_btn;
+    void initView(){
+        sign_up_btn = findViewById(R.id.sign_up_btn);
+        login_btn = findViewById(R.id.login_btn);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        initView();
+        sign_up_btn.setOnClickListener(v -> {
+            //Open Sign Up Activity
+            startActivity(new Intent(WelcomeActivity.this , SignUpActivity.class));
+        });
+        login_btn.setOnClickListener(v -> {
+            //TODO: Open Login Activity
+        });
     }
 }
