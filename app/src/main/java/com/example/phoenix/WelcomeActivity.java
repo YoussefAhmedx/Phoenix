31cd31aa3d1f5151ea4004899cb9f79fc5b776bd
Medhat <medhatassem.lf@gmail.com>
2021-01-14 04:56:24 +0200

    Fail broadcast ;(

--- app/src/main/java/com/example/phoenix/WelcomeActivity.java
+++ app/src/main/java/com/example/phoenix/WelcomeActivity.java
@@ -14,35 +14,31 @@ import android.widget.Button;
 public class WelcomeActivity extends AppCompatActivity {
 
     Button sign_up_btn, login_btn;
-    private BroadcastReceiver MyReceiver = null;
+//    private BroadcastReceiver MyReceiver = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        MyReceiver = new MyReceiver();
-        broadcastIntent();
+//        MyReceiver = new MyReceiver();
+//        broadcastIntent();
 
         // instantiating ViewPager
         ViewPager viewPager = findViewById(R.id.viewpager_container);
         //Setting the Adapter
         viewPager.setAdapter(new CustomPagerAdapter(this));
 
-        //TODO: Open SignUp Activity
+        //Open SignUp Activity
         sign_up_btn = findViewById(R.id.sign_up_btn);
-        sign_up_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
+        sign_up_btn.setOnClickListener(v -> startActivity(new Intent(WelcomeActivity.this, SignUpActivity.class)));
+    }
+//    public void broadcastIntent() {
+//        registerReceiver(MyReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+//    }
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        unregisterReceiver(MyReceiver);
+//    }
 
-            }
-        });
-    }
-    public void broadcastIntent() {
-        registerReceiver(MyReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        unregisterReceiver(MyReceiver);
-    }
+
 }
