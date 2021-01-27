a5668b2394a40c5eaec54ed6a5c22e782fa9e09e
YoussefAhmedx <56127360+YoussefAhmedx@users.noreply.github.com>
2021-01-27 18:10:50 +0200

    signInWith Google and firebase setup

--- app/src/main/java/com/example/phoenix/LoginLayout/LoginFragment.java
+++ app/src/main/java/com/example/phoenix/LoginLayout/LoginFragment.java
@@ -1,10 +1,13 @@
 package com.example.phoenix.LoginLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,11 +16,32 @@ import android.view.ViewGroup;
 import com.example.phoenix.R;
 import com.example.phoenix.SignUpLayout.AssistantFragment;
 import com.example.phoenix.SignUpLayout.SignUpDataFragment;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginFragment extends Fragment {
+    private static final String TAG ="test" ;
     //Variables
     CardView login_with_email_btn, login_with_google_btn , login_with_facebook_btn;
     Fragment previous_fragment;
+    GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 1;
+    private FirebaseAuth mAuth;
+
+
+
     Fragment fragment;
     public LoginFragment() {
         // Required empty public constructor
@@ -32,6 +56,12 @@ public class LoginFragment extends Fragment {
         login_with_email_btn = rootView.findViewById(R.id.login_with_e_mail_btn);
         login_with_google_btn = rootView.findViewById(R.id.login_with_google_btn);
         login_with_facebook_btn = rootView.findViewById(R.id.login_with_facebook_btn);
+        // Initialize Firebase Auth
+           mAuth = FirebaseAuth.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(getActivity(),gso);
 
         login_with_email_btn.setOnClickListener(v -> {
             //Login With E_mail
@@ -41,7 +71,8 @@ public class LoginFragment extends Fragment {
                     .addToBackStack(previous_fragment.getClass().getName()).commit();
         });
         login_with_google_btn.setOnClickListener(v -> {
-            //TODO: Login With Google
+            signIn();
+
         });
         login_with_facebook_btn.setOnClickListener(v -> {
             //TODO: Login with Facebook
@@ -50,10 +81,69 @@ public class LoginFragment extends Fragment {
         //TODO: Override  BackButton Event
 
 
-
-
-
-
         return  rootView;
     }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            Log.e(TAG, "handleSignInResult: "+"success" );
+            //startActivity(new Intent(getActivity(), MainActivity.class));
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+    }
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Snackbar.make(mBinding.mainLayout, "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
 }
